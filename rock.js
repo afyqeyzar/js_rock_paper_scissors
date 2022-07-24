cabd2a83773d146d68eb5825436eb92e1c866fda
afyqeyzar <norafyqe@gmail.com>
2022-07-24 12:19:05 +0100

    Added function to determine winner

--- rock.js
+++ rock.js
@@ -11,4 +11,39 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice)
+function playRound(playerSelection, computerSelection){
+    
+    let choice = playerSelection.toLowerCase()
+
+    if (choice === computerSelection){
+        return "its a tie :/"
+    }
+
+    else if(computerSelection === "rock"){
+        if (choice === "paper"){
+            return "You win, paper beats rock!"
+        }
+        else if (choice === "scissors"){
+            return "You lose, rock beats scissors!"
+        }
+    }
+
+    else if(computerSelection === "paper"){
+        if (choice === "scissors"){
+            return "You win, scissors beats paper!"
+        }
+        else if (choice === "rock"){
+            return "You lose, paper beats rock!"
+        }
+    }
+
+    /* if computer is scissors */
+    else {
+        if (choice === "rock"){
+            return "You win, rock beats scissors!"
+        }
+        else if (choice === "paper"){
+            return "You lose, scissors beats paper"
+        }
+    }
+}
