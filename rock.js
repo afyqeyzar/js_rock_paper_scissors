8ed148edcd97920a7c92227bd2f4ccb7766274fa
afyqeyzar <norafyqe@gmail.com>
2022-07-24 17:30:49 +0100

    added function game needs more work

--- rock.js
+++ rock.js
@@ -47,3 +47,32 @@ function playRound(playerSelection, computerSelection){
         }
     }
 }
+
+function game(){
+    let compScore = 0
+    let playerScore = 0
+
+    for (let i = 0; i < 5; i++){
+        let choice = prompt("Pick rock, paper or scissors")
+        console.log(getComputerChoice())
+        
+        let result = playRound(choice,getComputerChoice())
+
+        
+
+        if (result[4,6] == "win"){
+            return playerScore + 1
+        }
+
+        else if (result[4,7] == "lose"){
+            return compScore + 1
+        }
+
+        else{
+            return
+        }
+
+    }
+
+    console.log(compScore, playerScore)
+}
